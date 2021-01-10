ff4ed3e632b2b0c0165b70c5ff66028d4bb312fe
Nathan Holt <nathanwayneholt@gmail.com>
2021-01-10 11:18:15 -0500

    creation of assets/ and updated asset() helper function call to reflect so

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>CoM Danger Repo</title>
 
-        <link rel="text/css" href={{ asset('css/app.css') }} />
+        <link rel="text/css" href="{{ asset('assets/default.css') }}" />
     </head>
     <body>
         Testing. Can you see this?
